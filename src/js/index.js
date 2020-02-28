eb4247d7d25160d0795140da524a5e0cb4ddf0d5
Sergei Birukov <suxscribe@gmail.com>
2020-02-28 15:04:08 +0300

    content + fixes

---
+++ src/js/index.js
@@ -0,0 +1,291 @@
+
+import Pace from 'pace-progressbar';
+Pace.start();
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import Swiper from 'swiper';
+
+import Highway from '@dogstudio/highway';
+import {TimelineLite, CSSPlugin} from 'gsap/all';
+
+const plugins = [ CSSPlugin];
+
+// Force CSSPlugin to not get dropped during build
+// gsap.registerPlugin(CSSPlugin)
+
+// import 'pace-progressbar/themes/black/pace-theme-center-simple.css';
+
+// const paceOptions = {
+// 	ajax: true,
+// 	document:true,
+// 	eventLag: false
+// };
+
+if (document.querySelector('.page--index')) {
+	//Pace.start();
+
+	// Pace.on('done', function() {
+	// 	console.log('pace done');
+	// 	// Pace.stop();
+	// })
+	
+}
+
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+// import 'uikit/dist/css/uikit.min.css';
+// import './css/style.scss';
+
+
+// Page Transition FADE
+class Fade extends Highway.Transition {
+	in({from, to, done}) {
+
+		// const tl = new TimelineLite();
+		// tl.fromTo(to, 0.5, {left: '0%'}, {left: '0%', opacity: 0})
+		// .fromTo(to, 0.5, {opacity: 0 }, {opacity: 1, onComplete: function() {
+		// 	from.remove();
+		// 	done();
+		// }});
+
+		const tl = new TimelineLite();
+		// Reset Scroll
+		window.scrollTo(0, 0);
+
+		// Remove Old View
+		from.remove();
+	
+		// Animation
+		tl.fromTo(to, 0.5,
+		  { opacity: 0,
+			// x: '-100vw'
+		},
+		  {
+			opacity: 1,
+			// x: '0',
+			onComplete: done
+		  }
+		);
+
+		// Hangle Burger modal on page change
+		// var burger = document.getElementById('burger');
+		// const moveBurgerBack = (event) => {
+		// 	document.querySelector('.page__wrap').appendChild(burger);
+		// 	console.log('burger moved');
+		// }
+		// burger.addEventListener('hidden', moveBurgerBack, false);
+
+		// page scripts
+
+		pageScriptsLoad(); // reinit all scripts when switch to another page
+
+	}
+	out({from, done}) {
+
+
+		pageScriptsUnload();
+
+		// const moveBurgerBack = (event) => {
+		// 	document.querySelector('.page__wrap').appendChild(burger);
+		// 	console.log('burger moved');
+		// }
+		// burger.removeEventListener('hidden', moveBurgerBack, false);
+
+		// document.querySelector('.pace').remove();
+
+		console.log('moved');
+		const tl = new TimelineLite();
+		// Animation
+		tl.fromTo(from, 0.5,
+			{ opacity: 1,
+			//   x: 0
+			},
+			{
+			  opacity: 0,
+			//   x: '100vw',
+			  onComplete: done
+			}
+		);
+	}
+}
+
+// TRANSITION OVERLAP
+class Overlap extends Highway.Transition {
+	in({ from, to, done }) {
+	  // Reset Scroll
+	  window.scrollTo(0, 0);
+  
+	  const tl = new TimelineLite();
+
+	  // Animation
+	  tl.fromTo(to, 0.5,
+		{ 
+			x: '100vw',
+			// opacity: 0
+		},
+		{
+		  x: '0',
+		//   opacity: 1,
+		  onComplete: done
+		}, 0
+	  );
+  
+	  // Animation
+	  tl.fromTo(from, 0.5,
+		{ x: '0' },
+		{
+		  x: '-100vw',
+		  onComplete: () => {
+			// Set New View in DOM Stream
+			// to.style.position = 'static';
+  
+			// Remove Old View
+			from.remove();
+		  }
+		}, 0
+	  );
+
+	  pageScriptsLoad(); // reinit all scripts when switch to another page
+
+	}
+  
+	out({ done }) {
+		var burger = document.getElementById('burger');
+		burger.remove();
+
+		// TODO Add pageScriptsUnload(); 
+	  	done();
+	}
+  }
+  
+const H = new Highway.Core({
+	transitions: {
+		default: Fade
+		// default: Overlap
+	}
+});
+
+
+// SCROLL EVENT LISTENER
+
+const watchScroll = () => {
+	if (pageYOffset >= document.querySelector('.watch-scroll').offsetHeight) {
+		document.querySelector('.header').classList.add('header--invert');
+	}
+	else {
+		document.querySelector('.header').classList.remove('header--invert');
+	}
+}
+
+
+// SCRIPTS LOADED ON PAGE LOAD
+
+const pageScriptsLoad = () => {
+
+	// Preloader
+	var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+    console.log("TCL: number of images", len);
+
+	[].forEach.call( imgs, function( img ) {
+		if(img.complete)
+		incrementCounter();
+		else
+		img.addEventListener( 'load', incrementCounter, false );
+	} );
+
+	if (len == 0 ) {
+		hidePreloader();
+	}
+
+	function hidePreloader() {
+		if (document.querySelector('.preloader')) {
+			document.querySelector('.preloader').classList.remove('preloader--active');
+		}
+	}
+
+	function incrementCounter() {
+		counter++;
+		if ( counter === len ) {
+			console.log( 'All images loaded!' );
+			hidePreloader();
+		}
+	}
+
+	
+	// for index page only
+	if (document.querySelector('.page--index')) {
+			
+		var slideshowMain = new Swiper('.slideshow-nav', {
+			loop: true,
+			loopedSlides: 6,
+			slidesPerView: 'auto',
+			speed: 600,
+			grabCursor: true,
+			clickable: true, //zrx photoswipe
+			// Navigation arrows
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			allowTouchMove: true,
+			keyboard: {
+				enabled: true,
+			},
+			effect: 'slide',
+			centeredSlides: true,
+			// cssMode:true,
+
+		});
+		const slideshow = document.querySelector('.slideshow-nav');
+		const slideshowNavNext = document.querySelector('.slideshow-nav__nav-next');
+		const slideshowNavPrev = document.querySelector('.slideshow-nav__nav-prev');
+		const sun = document.querySelector('.index__sun');
+		const sunTitle = document.querySelector('.index__sun-title');
+		const sunIcon = document.querySelector('.index__sun-icon');
+		const logo = document.querySelector('.index__logo');
+		const logoTitle = document.querySelector('.index__logo-title');
+		const logoIcon = document.querySelector('.index__logo-icon');
+
+
+		const tween = new TimelineLite();
+		
+		tween.from(slideshow, 1.5, {scale: 0.7, opacity: 0});
+		tween.from(slideshowNavNext, 0.2, {scale: 0.7, opacity: 0, x: '-5vw'}, 1);
+		tween.from(slideshowNavPrev, 0.2, {scale: 0.7, opacity: 0, x: '5vw'}, 1);
+		
+		tween.from(sun, 1, {y: '-10vh', opacity: '0'}, 1.5);
+		tween.from(sunTitle, 0.2, {y: '-5vh', opacity:'0'}, 2);
+		tween.from(sunIcon, 0.3, {y: '-5vh', opacity:'0'}, 2);
+
+		tween.from(logo, 1, {y: '10vh', opacity: '0'}, 1.5);
+		tween.from(logoTitle, 0.2, {y: '5vh', opacity:'0'}, 2);
+		tween.from(logoIcon, 0.3, {y: '5vh', opacity:'0'}, 2);
+
+	}
+
+	if (document.querySelector('.watch-scroll')) {
+		console.log('watch-scroll');
+		window.addEventListener("scroll", watchScroll);
+	}
+
+};
+
+const pageScriptsUnload = () => {
+
+	window.removeEventListener('scroll', watchScroll);
+
+	//remove burger menu to where it belongs from <body>
+	var burger = document.getElementById('burger');
+	burger.remove();
+
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	pageScriptsLoad();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,123 +6,242 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin= require('copy-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
-/*function generateHtmlPlugins(templateDir) {
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles.map(item => {
-	const parts = item.split('.');
-	const name = parts[0];
-	const extension = parts[1];
-	return new HtmlWebpackPlugin({
-	  filename: `${name}.html`,
-	  template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-	  inject: false,
-	})
-  })
-}*/
+function generateHtmlPlugins(templateDir) {
+	const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+	return templateFiles.map(item => {
+	  const parts = item.split(".");
+	  const name = parts[0];
+	  const extension = parts[1];
+	  return new HtmlWebpackPlugin({
+		filename: `${name}.html`,
+		template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`)
+		// inject: false //don't add js and css to html
+	  });
+	});
+}
 
-// const htmlPlugins = generateHtmlPlugins('./src/html/views')
 
-module.exports = {
-	entry: './src/index.js',
+const htmlPlugins = generateHtmlPlugins('./src/html/views')
+
+const config = {
+	entry: ["./src/js/index.js", "./src/scss/style.scss"],
 	output: {
-		// mode: 'development', // 'production' = -p key
-		filename: './js/main.js',
-		path: path.resolve(__dirname,'dist'),
-		// publicPath: 'http://suxscribe.tmweb.ru/transsignal/'
+	  filename: "./js/bundle.js",
+	  // publicPath: 'http://suxscribe.tmweb.ru/transsignal/'
 	},
-	devtool: 'source-map',
-	devServer: {
-		contentBase: './dist'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/html/views/index.html',
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'project.html',
-			template: './src/html/views/project.html',
-		}),
-
-		new MiniCssExtractPlugin({
-			// Options similar to the same options in webpackOptions.output
-			// all options are optional
-			filename: './css/[name].css', // создает одноименный файл
-		}),
-		new CopyWebpackPlugin([{
-				from: './src/fonts',
-				to: './fonts'
-			  },
-			  {
-				from: './src/favicon',
-				to: './favicon'
-			  },
-			  {
-				from: './src/assets',
-				to: './assets'
-			  },
-			  /*{
-				from: './src/uploads',
-				to: './uploads'
-			  }*/
-		]),
-		/*new CleanWebpackPlugin({
-			cleanOnceBeforeBuildPatterns:['./dist/*.*'] //remove everything before build
-		}
-		),*/
-		/*new OptimizeCssAssetsPlugin({
-			assetNameRegExp: /\.css$/g,
-			cssProcessor: require('cssnano'),
-			cssProcessorPluginOptions: {
-				preset: ['default', { discardComments: { removeAll: true } }], //remove comments
-			},
-			canPrint: true
-		})*/
-
-	], //.concat(htmlPlugins)
+	devtool: "source-map",
+	mode: "production",
+	// optimization: {
+	//   minimizer: [
+	//     new TerserPlugin({
+	//       sourceMap: true,
+	//       extractComments: true
+	//     })
+	//   ]
+	// },
 	module: {
-		rules: [
-
-			{ test: /\.html$/,
-			  include: path.join(__dirname, 'src/html/includes'),
-			  use: {
-			    loader: 'html-loader',
-			    options: {
-			      interpolate: true
-			    }
+	  rules: [
+		
+		{
+		  test: /\.(sass|scss)$/,
+		  include: path.resolve(__dirname, "src/scss"),
+		  use: [
+			{
+			  loader: MiniCssExtractPlugin.loader,
+			  options: {}
+			},
+			{
+			  loader: "css-loader",
+			  options: {
+				sourceMap: true,
+				url: false
+			  }
+			},
+  
+			{
+			  loader: "postcss-loader",
+			  options: {
+				ident: "postcss",
+				sourceMap: true,
+				plugins: () => [
+				  require("cssnano")({
+					preset: [
+					  "default",
+					  {
+						discardComments: {
+						  removeAll: true
+						}
+					  }
+					]
+				  })
+				]
 			  }
 			},
 			{
-				test: /\.scss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader'
-				]
-			},
-			{
-				test: /\.(jpg|png|svg|gif|webp)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: './assets/',
-							useRelativePath: true
-						}
-					},
-					/*{ //optimize images
-						loader: 'image-webpack-loader',
-						options: {
-							mozjpeg: {
-								progressive: true,
-								quality: 70
-							}
-						}
-					}*/
-				]
-			},
-			{ test: /\.(woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=100000' }
-		],
+			  loader: "sass-loader",
+			  options: {
+				sourceMap: true
+			  }
+			}
+		  ]
+		},
+		{
+		  test: /\.html$/,
+		  include: path.resolve(__dirname, "src/html/includes"),
+		  use: ["raw-loader"]
+		}
+	  ]
 	},
-}
+	plugins: [
+	  new MiniCssExtractPlugin({
+		filename: "./css/style.bundle.css"
+	  }),
+	  new CopyWebpackPlugin([
+		{
+		  from: "./src/fonts",
+		  to: "./fonts"
+		},
+		{
+		  from: "./src/favicon",
+		  to: "./favicon"
+		},
+		{
+		  from: "./src/assets",
+		  to: "./assets"
+		}
+	  ])
+	].concat(htmlPlugins)
+  };
+
+module.exports = (env, argv) => {
+	externals: {
+	}
+	if (argv.mode === "production") {
+		config.plugins.push(new CleanWebpackPlugin());
+	}
+	return config;
+};
+
+
+
+// module.exports = {
+// 	entry: './src/index.js',
+// 	output: {
+// 		// mode: 'development', // 'production' = -p key
+// 		filename: './js/main.js', //[hash]
+// 		path: path.resolve(__dirname,'dist'),
+// 		// publicPath: 'http://suxscribe.tmweb.ru/transsignal/'
+// 	},
+// 	devtool: 'source-map',
+// 	devServer: {
+// 		contentBase: './dist'
+// 	},
+// 	plugins: [
+// 		new HtmlWebpackPlugin({
+// 			template: './src/html/views/index.html',
+// 		}),
+// 		new HtmlWebpackPlugin({
+// 			filename: 'project.html',
+// 			template: './src/html/views/project.html',
+// 		}),
+// 		new HtmlWebpackPlugin({
+// 			filename: 'content.html',
+// 			template: './src/html/views/content.html',
+// 		}),
+// 		new HtmlWebpackPlugin({
+// 			filename: 'content-list.html',
+// 			template: './src/html/views/content-list.html',
+// 		}),
+
+// 		new MiniCssExtractPlugin({
+// 			// Options similar to the same options in webpackOptions.output
+// 			// all options are optional
+// 			filename: './css/[name].css', // создает одноименный файл // .[hash]
+// 		}),
+// 		new CopyWebpackPlugin([{
+// 				from: './src/fonts',
+// 				to: './fonts'
+// 			  },
+// 			  {
+// 				from: './src/favicon',
+// 				to: './favicon'
+// 			  },
+// 			  {
+// 				from: './src/assets',
+// 				to: './assets'
+// 			  },
+// 			  /*{
+// 				from: './src/uploads',
+// 				to: './uploads'
+// 			  }*/
+// 		]),
+// 		new CleanWebpackPlugin({
+// 			cleanOnceBeforeBuildPatterns:['**/*.css*','**/*.js*'] //remove everything before build
+// 		}
+// 		),
+// 		new webpack.LoaderOptionsPlugin({
+// 			options: {
+// 				postcss: [
+// 					autoprefixer()
+// 				]
+// 			}
+// 		})
+// 		/*new OptimizeCssAssetsPlugin({
+// 			assetNameRegExp: /\.css$/g,
+// 			cssProcessor: require('cssnano'),
+// 			cssProcessorPluginOptions: {
+// 				preset: ['default', { discardComments: { removeAll: true } }], //remove comments
+// 			},
+// 			canPrint: true
+// 		})*/
+
+// 	], //.concat(htmlPlugins)
+// 	module: {
+// 		rules: [
+
+// 			{ test: /\.html$/,
+// 			  include: path.join(__dirname, 'src/html/includes'),
+// 			  use: {
+// 			    loader: 'html-loader',
+// 			    options: {
+// 			      interpolate: true
+// 			    }
+// 			  }
+// 			},
+// 			{
+// 				test: /\.(scss|css)$/,
+// 				use: [
+// 					MiniCssExtractPlugin.loader,
+// 					'css-loader',
+// 					'postcss-loader',
+// 					'sass-loader'
+// 				]
+// 			},
+// 			// {
+// 			// 	test: /\.(jpg|png|svg|gif|webp)$/,
+// 			// 	use: [
+// 			// 		{
+// 			// 			loader: 'file-loader',
+// 			// 			options: {
+// 			// 				name: '[name].[ext]',
+// 			// 				outputPath: './assets/',
+// 			// 				useRelativePath: true
+// 			// 			}
+// 			// 		},
+// 			// 		/*{ //optimize images
+// 			// 			loader: 'image-webpack-loader',
+// 			// 			options: {
+// 			// 				mozjpeg: {
+// 			// 					progressive: true,
+// 			// 					quality: 70
+// 			// 				}
+// 			// 			}
+// 			// 		}*/
+// 			// 	]
+// 			// },
+// 			{ test: /\.(woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=100000' }
+// 		],
+// 	},
+// }
